d8be7e6e6cc5d902277f9d78acefe21efe665377
Alexander Kaczmarek <k.alexander@live.com>
2018-07-16 16:29:19 +0000

    Changed the way adding players in multiplayer mode was handled, creating a data file based on game room name entered on create-room.html page to hold list of added players. Added more tests and organized the code slightly for readability.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from utils.utils import *
 app = Flask(__name__)
 
 file_path_single = "data/users.json"
-file_path_multi = "data/multi_users.json"
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -14,19 +13,11 @@ def index():
     remove_user_from_data_file(file_path_single, "testUser")
     remove_user_from_data_file(file_path_single, "testUser2")
     
-    with open(file_path_multi, "r") as json_data:
-        player_list = json.load(json_data)
-        
-    player_list = []
-    
-    with open(file_path_multi, "w") as json_data:
-        json.dump(player_list, json_data)
-    
     if request.method == "POST":
         if request.form["action"] == "Play alone":
             return redirect(url_for("signIn"))
         else:
-            return redirect(url_for("addPlayers"))
+            return redirect(url_for("createRoom"))
     return render_template("index.html")
 
 
@@ -38,7 +29,7 @@ def signIn():
     if request.method == "POST":
         if request.form["action"] == "Enter game":
             if check_if_password_matches(file_path_single, request.form["username"], request.form["password"]):
-                return redirect(url_for("gameMenu"))
+                return redirect(url_for("gameMenu", username=request.form["username"]))
             elif check_if_username_does_exist(file_path_single, request.form["username"]):
                 username_error = "A user with this username does not exist"
             else:
@@ -56,42 +47,53 @@ def signUp():
             add_user_to_data_file(file_path_single, request.form["username"], request.form["password"])
             return redirect(url_for("gameMenu"))
         else:
-            username_error = "This username is taken"
+            username_error = "Username is taken"
     return render_template("sign-up.html", username_error=username_error)
 
 
    
-@app.route("/game-menu", methods=["GET", "POST"])
-def gameMenu():
+@app.route("/game-menu/<username>", methods=["GET", "POST"])
+def gameMenu(username):
     return render_template("game-menu.html")
-    
 
+
+
+@app.route("/create-room", methods=["GET", "POST"])
+def createRoom():
+    if request.method == "POST":
+        create_data_file(request.form["game_name"])
+        return redirect(url_for("addPlayers", game_name=game_name))
+    return render_template("create-room.html")
+
+ 
     
-@app.route("/add-players", methods=["GET", "POST"])
-def addPlayers():
+@app.route("/add-players/<game_name>", methods=["GET", "POST"])
+def addPlayers(game_name):
     username_error = None
     number_of_players_error = None
     
-    with open(file_path_multi, "r") as json_data:
+    with open(create_file_path(game_name), "r") as json_data:
         player_list = json.load(json_data)
         
-    
     if request.method == "POST":
-        if "remove_player" in request.form:
-            remove_last_user_from_multi_data_file(file_path_multi)
-            return redirect(url_for("addPlayers"))
+        for i in range(5):
+            if str(i) in request.form:
+                remove_user_from_data_by_index(create_file_path(game_name), i)
+                return redirect(url_for("addPlayers", game_name=game_name))
+        if "action" in request.form:
+            delete_json_file(game_name)
+            return redirect(url_for("gameMulti"))
         elif "username" in request.form:
-            if check_number_of_multi_users(file_path_multi) == False:
-                if check_if_username_does_exist(file_path_multi, request.form["username"]):
-                    add_user_to_data_file(file_path_multi, request.form["username"], "none")
-                    return redirect(url_for("addPlayers"))
+            if check_number_of_multi_users(create_file_path(game_name)) == False:
+                if check_if_username_does_exist(create_file_path(game_name), request.form["username"]):
+                    add_user_to_data_file(create_file_path((game_name)), request.form["username"], "none")
+                    return redirect(url_for("addPlayers", game_name=game_name))
                 else:
-                    username_error = "Player already exists"
+                    username_error = "Username is taken"
             else:
                 number_of_players_error = "The maximum number of players has been reached"
             
-        
-    return render_template("multiplayer.html", username_error=username_error, number_of_players_error=number_of_players_error, player_list=player_list)
+    return render_template("add-players.html", username_error=username_error, number_of_players_error=number_of_players_error, player_list=player_list)
 
 
 
--- test.py
+++ test.py
@@ -44,24 +44,15 @@ class app_tests(unittest.TestCase):
         
     
     
-    """ Test whether password matches the username """
+    """ Test whether input password matches the password for that username """
     def test_if_password_matches_username(self):
         add_user_to_data_file("data/users.json", "testUser", "ka10")
         user_one_password_not_match = check_if_password_matches("data/users.json", "testUser", "ka9")
         user_one_password_matches = check_if_password_matches("data/users.json", "testUser", "ka10")
         
-        add_user_to_data_file("data/users.json", "testUser2", "ka10waii")
-        user_two_password_not_match = check_if_password_matches("data/users.json", "testUser", "***SSS")
-        user_two_password_matches = check_if_password_matches("data/users.json", "testUser2", "ka10waii")
-        user_three_password_matches = check_if_password_matches("data/users.json", "falon", "ka10waii")
-        
         self.assertFalse(user_one_password_not_match)
         self.assertTrue(user_one_password_matches)
-        
-        self.assertFalse(user_two_password_not_match)
-        self.assertTrue(user_two_password_matches)
-        
-        self.assertFalse(user_three_password_matches)
+
         
     
     
@@ -75,11 +66,96 @@ class app_tests(unittest.TestCase):
                 return True
             else:
                 return False
+        
                 
                 
         self.assertFalse(does_exists)
         
         self.assertFalse(check_if_added())
+    
+    
+       
+    """ Test whether a data file can be created """
+    def test_if_data_file_can_be_created(self):
+        create_data_file("testFile")
+                
+        self.assertTrue(check_if_file_exists("testFile"))
+    
+    
+    
+    """ Test whether a data file can be deleted """
+    def test_if_data_file_can_be_deleted(self):
+        delete_data_file("testFile")
+        
+        self.assertFalse(check_if_file_exists("testFile"))
+        
+    
+        
+    """ Test whether a string containing the path to a file can be created """
+    def test_if_string_path_to_file_can_be_created(self):
+        create_data_file("testFile2")
+        
+        file_path = create_file_path("testFile2")
+        
+        self.assertEqual("data/testFile2.json", file_path)
+        self.assertEqual(str, type(file_path))
+        
+        delete_data_file("testFile2")
+        
+    
+    
+    """ Test whether a user object can be removed from data file by its index number """
+    def test_if_can_remove_user_by_index(self):
+        create_data_file("testUsersList")
+        file_path = create_file_path("testUsersList")
+        add_user_to_data_file(file_path, "testUser", "none")
+        add_user_to_data_file(file_path, "testUser2", "none")
+        
+        userIndexZero = create_user("testUser", "none", 0)
+        userIndexOne = create_user("testUser2", "none", 0)
+        
+        with open(file_path, "r") as json_data:
+            data = json.load(json_data)
+        
+        self.assertEqual(data[1], userIndexOne)
+        
+        remove_user_from_data_by_index(file_path, 0)
+        
+        with open(file_path, "r") as json_data:
+            data = json.load(json_data)
+            
+        self.assertEqual(data[0], userIndexOne)
+        
+        delete_data_file("testUsersList")
+        
+    """ Test whether a data file containing 5 users actually contains 5 users, assert True if yes and False if not """
+    def test_if_data_contains_five_users(self):
+        create_data_file("testUsersList")
+        file_path = create_file_path("testUsersList")
+        add_user_to_data_file(file_path, "testUser", "none")
+        add_user_to_data_file(file_path, "testUser2", "none")
+        add_user_to_data_file(file_path, "testUser3", "none")
+        add_user_to_data_file(file_path, "testUser4", "none")
+        add_user_to_data_file(file_path, "testUser5", "none")
+        
+        create_data_file("testUsersList2")
+        file_path2 = create_file_path("testUsersList2")
+        add_user_to_data_file(file_path, "testUser", "none")
+        add_user_to_data_file(file_path, "testUser2", "none")
+        add_user_to_data_file(file_path, "testUser3", "none")
+        
+            
+        self.assertTrue(check_number_of_multi_users(file_path))
+        self.assertFalse(check_number_of_multi_users(file_path2))
+        
+        delete_data_file("testUsersList")
+        delete_data_file("testUsersList2")
+        
+        
+        
+        
+       
+        
         
         
         
