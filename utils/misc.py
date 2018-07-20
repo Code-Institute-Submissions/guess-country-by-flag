05aeac5609e9ef80b602bba27ebdba43ca4b5694
Alexander Kaczmarek <k.alexander@live.com>
2018-07-20 17:22:30 +0000

    Added function to generate a list of a given number of random numbers

---
+++ utils/misc.py
@@ -0,0 +1,36 @@
+import random
+
+
+
+
+""" Generate a list of a given amount of index numbers between 0 and 204 """
+def generate_random_numbers(amount):
+    list_of_index_numbers = []
+    
+    for i in range(amount):
+        random_number = random.randint(0,204)
+        list_of_index_numbers.append(random_number)
+    
+    return list_of_index_numbers
+
+
+        
+
+
+
+
+            
+            
+
+
+
+
+
+
+                        
+                        
+        
+
+
+    
+    
