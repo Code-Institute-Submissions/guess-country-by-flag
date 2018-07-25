5dbc8855d7272760ff3b715baab974b6d27fc0cc
Alexander Kaczmarek <k.alexander@live.com>
2018-07-25 00:08:31 +0000

    Added leaderboard feature and finised off single play mode, refactored code and added some more styles

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
 import json
-from flask import Flask, render_template, request, redirect, url_for 
-from utils import countries, file, game_single, misc, users
 import random
+import time
+from flask import Flask, render_template, request, redirect, url_for 
+from utils import countries, files, game_single, misc, users, leaderboard_single
+
 
 app = Flask(__name__)
 
-file_path_single = "data/users.json"
+
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -25,16 +27,19 @@ def index():
 def signIn():
     username_error = None
     password_error = None
+    
     if request.method == "POST":
         if request.form["action"] == "Enter game":
-            if users.check_if_password_matches(file_path_single, request.form["username"], request.form["password"]):
-                return redirect(url_for("gameSingle", username=request.form["username"]))
-            elif users.check_if_username_does_exist(file_path_single, request.form["username"]):
+            if users.check_if_password_matches("users", request.form["username"], request.form["password"]):
+                return redirect(url_for("gameMenu", username=request.form["username"]))
+            elif users.check_if_username_does_exist("users", request.form["username"]):
                 username_error = "A user with this username does not exist"
             else:
                 password_error = "Wrong password, try again"
             
-    return render_template("sign-in.html", password_error=password_error, username_error=username_error)
+    return render_template("sign-in.html",
+                            password_error=password_error,
+                            username_error=username_error)
 
 
    
@@ -42,19 +47,28 @@ def signIn():
 def signUp():
     username_error = None
     list_of_countries = []
+    
     if request.method == "POST":
-        if users.check_if_username_does_exist(file_path_single, request.form["username"]):
-            users.add_user_to_data_file(file_path_single, request.form["username"], request.form["password"], game_single.create_game_object())
+        if users.check_if_username_does_exist("users", request.form["username"]):
+            users.add_user_to_data_file("users", request.form["username"], request.form["password"], game_single.create_game_object())
             
-            return redirect(url_for("gameSingle", username=request.form["username"]))
+            return redirect(url_for("gameMenu", username=request.form["username"]))
         else:
             username_error = "Username is taken"
-    return render_template("sign-up.html", username_error=username_error)
+    return render_template("sign-up.html",
+                            username_error=username_error)
 
 
    
 @app.route("/game-menu/<username>", methods=["GET", "POST"])
 def gameMenu(username):
+    if request.method == "POST":
+        if "play" in request.form:
+            return redirect(url_for("gameSingle", username=username))
+        elif "leaderboard" in request.form:
+            return redirect(url_for("leaderboard", username=username))
+        else:
+            return redirect(url_for("index"))
     return render_template("game-menu.html")
 
 
@@ -62,7 +76,7 @@ def gameMenu(username):
 @app.route("/create-room", methods=["GET", "POST"])
 def createRoom():
     if request.method == "POST":
-        file.create_data_file(request.form["game_name"])
+        files.create_data_file(request.form["game_name"])
         return redirect(url_for("addPlayers", game_name=request.form["game_name"]))
     return render_template("create-room.html")
 
@@ -72,70 +86,136 @@ def createRoom():
 def addPlayers(game_name):
     username_error = None
     number_of_players_error = None
-    
-    with open(file.create_file_path(game_name), "r") as json_data:
-        player_list = json.load(json_data)
+        
+    player_list = files.read_data_file(game_name)
         
     if request.method == "POST":
         for i in range(5):
             if str(i) in request.form:
-                users.remove_user_from_data_by_index(file.create_file_path(game_name), i)
+                users.remove_user_from_data_by_index(game_name, i)
                 return redirect(url_for("addPlayers", game_name=game_name))
+                
         if "action" in request.form:
-            file.delete_data_file(game_name)
+            files.delete_data_file(game_name)
             return redirect(url_for("gameMulti"))
         elif "username" in request.form:
-            if users.check_number_of_multi_users(file.create_file_path(game_name)) == False:
-                if users.check_if_username_does_exist(file.create_file_path(game_name), request.form["username"]):
-                    users.add_user_to_data_file(file.create_file_path((game_name)), request.form["username"], "none", game_single.create_game_object())
+            if users.check_number_of_multi_users(game_name) == False:
+                if users.check_if_username_does_exist(game_name, request.form["username"]):
+                    users.add_user_to_data_file(game_name, request.form["username"], "none", game_single.create_game_object())
                     return redirect(url_for("addPlayers", game_name=game_name))
                 else:
                     username_error = "Username is taken"
             else:
                 number_of_players_error = "The maximum number of players has been reached"
             
-    return render_template("add-players.html", username_error=username_error, number_of_players_error=number_of_players_error, player_list=player_list)
+    return render_template("add-players.html",
+                            username_error=username_error,
+                            number_of_players_error=number_of_players_error,
+                            player_list=player_list)
 
 
 
 @app.route("/game-single/<username>", methods=["GET", "POST"])
 def gameSingle(username):
-    
     user_score = users.return_user_score(username)
-    
-    question_num = game_single.extract_key_value_from_game_obj(username, "question_number")
-    list_of_countries = game_single.extract_key_value_from_game_obj(username, "countries")
+    question_number = game_single.extract_key_value_from_game_obj(username, "question_number")
     points = game_single.extract_key_value_from_game_obj(username, "points")
     incorrect_answers = game_single.extract_key_value_from_game_obj(username, "incorrect_answers")
     
-    round_country = list_of_countries[question_num - 1]
+    list_of_countries = game_single.extract_key_value_from_game_obj(username, "countries")
+    country_for_current_round = list_of_countries[question_number - 1]
     
-    flag = countries.return_value_for_key_in_country_object_of_index("testCountries", round_country, "flag")
-    official_name = countries.return_value_for_key_in_country_object_of_index("testCountries", round_country, "name-official")
-    common_name = countries.return_value_for_key_in_country_object_of_index("testCountries", round_country, "name-common")
+    flag = countries.return_value_for_key_in_country_object_of_index("countries", country_for_current_round, "flag")
+    official_name = countries.return_value_for_key_in_country_object_of_index("countries", country_for_current_round, "name-official")
+    common_name = countries.return_value_for_key_in_country_object_of_index("countries", country_for_current_round, "name-common")
+    
+    
+    
+    if question_number == 1:
+        start_time = time.time()
+        game_single.set_game_duration(username, start_time)
+    
+    if points == 0:
+        game_single.increase_question_number(username)
+        game_single.reset_incorrect_answers(username)
+        game_single.reset_round_points(username)
+        return redirect(url_for("gameSingle", username=username))
+        
     
     if request.method == "POST":
-        if request.form["answer"] == official_name or request.form["answer"] == common_name:
-            game_single.increase_question_number(username)
-            users.increase_user_score(username, points)
-            game_single.reset_incorrect_answers(username)
-            game_single.reset_round_points(username)
-            return redirect(url_for("gameSingle", username=username))
-        else:
-            game_single.append_incorrect_answers(username, request.form["answer"])
-            game_single.decrease_round_points(username)
-            return redirect(url_for("gameSingle", username=username))
-        
-        
-        
+        if request.form["answer"]:
+            answer = misc.answer_to_lowercase_then_titlecase(request.form["answer"])
+            
+            if question_number == 20 and (answer == official_name or answer == common_name):
+                end_time = time.time()
+                game_single.check_for_win(user_score, username)
+                game_single.set_game_duration(username, round(misc.calculate_game_duration(start_time, end_time, username), 2))
+                users.increase_user_score(username, points)
+                player_leaderboard = leaderboard_single.create_leaderboard_player(username)
+                files.append_to_file(player_leaderboard, "leaderboard")
+                leaderboard_single.update_leaderboard("leaderboard")
+                return redirect(url_for("leaderboard", username=username))
+                
+            else:
+                if answer == official_name or answer == common_name:
+                    game_single.increase_question_number(username)
+                    users.increase_user_score(username, points)
+                    game_single.reset_incorrect_answers(username)
+                    game_single.reset_round_points(username)
+                    return redirect(url_for("gameSingle", username=username))
+                    
+                else:
+                    game_single.append_incorrect_answers(username, request.form["answer"])
+                    game_single.decrease_round_points(username)
+                    return redirect(url_for("gameSingle", username=username))
+                    
+        elif "give-up" in request.form or "play-again" in request.form:
+            game_single.set_win_state(username, "lost")
+            return redirect(url_for("leaderboard", username=username))
+            
+        elif "skip" in request.form:
+                game_single.increase_question_number(username)
+                game_single.reset_incorrect_answers(username)
+                game_single.reset_round_points(username)
+                return redirect(url_for("gameSingle", username=username)) 
+            
+    
     return render_template("game-single.html", 
                             username=username,
                             flag_url=flag,
-                            question_number=question_num,
+                            question_number=question_number,
                             round_points=points,
                             incorrect_answers=incorrect_answers,
                             score=user_score)
     
+    
+    
+@app.route("/leaderboard/<username>", methods=["GET", "POST"])
+def leaderboard(username):
+    top_ten = leaderboard_single.update_top_ten("leaderboard")
+    win = game_single.extract_key_value_from_game_obj(username, "win")
+    user_score = users.return_user_score(username)
+    time = leaderboard_single.return_user_game_duration(username, "leaderboard")
+    
+    game_single.reset_question_number(username)
+    game_single.reset_incorrect_answers(username)
+    game_single.reset_round_points(username)
+    game_single.generate_new_list_of_random_countries(username)
+    users.reset_user_score(username)
+    
+    if request.method == "POST":
+        game_single.set_win_state(username, "not-won")
+        if "play-again" in request.form:
+            return redirect(url_for("gameSingle", username=username))
+        elif "continue" in request.form:
+            return redirect(url_for("leaderboard", username=username))
+            
+    return render_template("leaderboard-single.html",
+                            username=username, win=win,
+                            score=user_score,
+                            top_ten=top_ten,
+                            time=time)
+
     
 if __name__ == "__main__":
     app.run(host = os.environ.get("IP"),
