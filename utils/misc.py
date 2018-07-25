51176d24dd2ab6914d85e6a86e18f5d88750b2e1
Alexander Kaczmarek <k.alexander@live.com>
2018-07-25 00:01:38 +0000

    Added functions to calculate elapsed time based on start and end time points and a function to convert strings to lower and then title case

--- utils/misc.py
+++ utils/misc.py
@@ -1,17 +1,34 @@
 import random
+import time
+import game_single
+
 
 
 
 
 """ Generate a list of a given amount of index numbers between 0 and 204 """
 def generate_random_numbers(amount):
-    list_of_index_numbers = []
+    list_of_index_numbers = None
     
-    for i in range(amount):
-        random_number = random.randint(0,204)
-        list_of_index_numbers.append(random_number)
+    list_of_index_numbers = random.sample(range(204), amount)
     
     return list_of_index_numbers
+    
+""" Calculate game_duration """ 
+def calculate_game_duration(start_time, end_time, username):
+    elapsed_time = end_time - start_time
+    
+    return elapsed_time
+
+
+""" Convert user input to lowercase and then to titlecase """            
+def answer_to_lowercase_then_titlecase(user_input):
+    answer_unmodified = user_input
+    answer_to_lowercase = user_input.lower()
+    answer_to_titlecase = answer_to_lowercase.title()
+        
+    return answer_to_titlecase
+                
 
 
         
